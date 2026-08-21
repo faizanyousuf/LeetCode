3094b32e1a9b5f684c8532e3d45334b426d1f07e
Faizan Yousuf <reshifaixxan980@gmail.com>
2026-08-21 14:42:55 +0530

    add smallest window containing 0 1 2 optimized version

--- SlidingWindow/Easy/SmallestWindowContaining012.java
+++ SlidingWindow/Easy/SmallestWindowContaining012.java
@@ -41,6 +41,41 @@
 public class SmallestWindowContaining012{
     public static void main(String[] args){
 
-        
+
     }
 }
+class Solution {
+    public int smallestSubstring(String s) {
+        // code here
+        
+        int zeroIndex = -1;
+        int oneIndex = -1;
+        int twoIndex = -1;
+        int minLength = Integer.MAX_VALUE;
+        boolean found = false;
+        for(int i = 0; i < s.length();i++){
+            char ch = s.charAt(i);
+            
+            if(ch == '0'){
+                zeroIndex = i;
+            }else if(ch == '1'){
+                oneIndex = i;
+            }else{
+                twoIndex = i;
+            }
+            
+            if(zeroIndex != -1 && oneIndex != -1 && twoIndex != -1){
+                 found = true;
+                 int index[] = {zeroIndex,oneIndex,twoIndex};
+                 Arrays.sort(index);
+                 int length = index[2] -index[0] +1;
+                 
+                 if(minLength > length){
+                     minLength = length;
+                 }
+            }
+        }
+         minLength = (found) ? minLength : -1;
+        return minLength;
+    }
+};
