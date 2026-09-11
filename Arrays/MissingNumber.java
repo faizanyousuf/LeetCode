b1f85163a4bb5fc523fdce6759c955ae2574d8b7
Faizan Yousuf <reshifaixxan980@gmail.com>
2026-09-11 15:34:31 +0530

    add missing number

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int n = nums.length;
+
+        int sum = (n*(n+1))/2;
+        
+        int arrSum = 0;
+        for(int val : nums){
+               arrSum += val;
+        }
+        int missing = sum-arrSum;
+        return missing;
+    }
+}
